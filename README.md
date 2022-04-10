e9c7594be11c8b3b3075583709f182b0f229b45d
utr1903 <ugurturkarslan@gmail.com>
2022-04-10 11:37:43 +0200

    Add k8s

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kafka-tutorial
+
+## Reference
+
+### Kafka K8s
+https://github.com/Yolean/kubernetes-kafka
---
+++ Runner-2.ps1
@@ -0,0 +1,32 @@
+### Test Kafka server
+
+## Zookeeper Server 0
+
+# Build
+docker build --tag utr1903/zookeeper-0 .\zookeeper-0\.
+
+# Run
+docker push utr1903/zookeeper-0
+
+## Kafka Server 0
+
+# Build
+docker build --tag utr1903/kafka-0 .\kafka-0\.
+
+# Run
+docker push utr1903/kafka-0
+
+## Topic
+
+# Create
+kubectl exec kafka-0-0 -n kafka -it -- bash `
+    /kafka/bin/kafka-topics.sh `
+    --bootstrap-server kafka-0:9092 `
+    --create `
+    --topic first
+
+# Producer
+# bash kafka-console-producer.sh --bootstrap-server kafka-0:9092 --topic first
+
+# Consumer
+# bash kafka-console-consumer.sh --bootstrap-server kafka-0:9092 --topic first
