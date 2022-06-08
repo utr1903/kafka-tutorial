ef37b8504218e36fdf43296b5c5e6b0d103c69a5
uturkarslan <ugurturkarslan@gmail.com>
2022-06-08 22:01:32 +0200

    Add Kafka headers

--- apps/consumer/src/main/java/com/kafka/tutorial/consumer/listener/MyTopicListener.java
+++ apps/consumer/src/main/java/com/kafka/tutorial/consumer/listener/MyTopicListener.java
@@ -1,11 +1,18 @@
 package com.kafka.tutorial.consumer.listener;
 
 import com.kafka.tutorial.consumer.constant.Constants;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class MyTopicListener {
+
+    private final Logger logger = LoggerFactory.getLogger(MyTopicListener.class);
 
     private static final String TOPIC = "mytopic";
 
@@ -14,8 +21,15 @@ public class MyTopicListener {
     }
 
     @KafkaListener(topics = TOPIC, groupId = Constants.GROUP_ID)
-    public void listen(String message)
+    public void listen(ConsumerRecord<String, String> record)
     {
-        System.out.println(message);
+        for (var header : record.headers()) {
+            String value = new String(header.value());
+            logger.info("Header key  : " + header.key());
+            logger.info("Header value: " + value);
+        }
+
+        logger.info("Message key: " + record.key());
+        logger.info("Message value: " + record.value());
     }
 }
--- apps/producer/src/main/java/com/kafka/tutorial/producer/service/kafka/KafkaService.java
+++ apps/producer/src/main/java/com/kafka/tutorial/producer/service/kafka/KafkaService.java
@@ -2,10 +2,17 @@ package com.kafka.tutorial.producer.service.kafka;
 
 import com.kafka.tutorial.producer.dtos.KafkaRequestDto;
 import com.kafka.tutorial.producer.dtos.KafkaResponseDto;
+import com.newrelic.telemetry.SpanBatchSenderFactory;
+import com.newrelic.telemetry.spans.SpanBatchSender;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +21,9 @@ import java.util.Properties;
 @Service
 public class KafkaService {
 
-    KafkaProducer<String, String> producer;
+    private final Logger logger = LoggerFactory.getLogger(KafkaService.class);
+
+    private KafkaProducer<String, String> producer;
 
     public KafkaService()
     {
@@ -27,11 +36,31 @@ public class KafkaService {
     {
         KafkaResponseDto responseDto = new KafkaResponseDto();
 
+        Headers headers = new RecordHeaders();
+        headers.add(new RecordHeader("myHeader", "myHeader".getBytes()));
+
         ProducerRecord<String, String> record =
                 new ProducerRecord<>(
                     requestDto.getTopic(),
-                    requestDto.getValue()
+                    null,
+                    "key",
+                    requestDto.getValue(),
+                    headers
                 );
+
+        for (var header : record.headers()) {
+            String value = new String(header.value());
+            logger.info("Header key  : " + header.key());
+            logger.info("Header value: " + value);
+        }
+
+//        SpanBatchSender sender =
+//                SpanBatchSender.create(
+//                        SpanBatchSenderFactory.fromHttpImplementation(OkHttpPoster::new)
+//                                .configureWith(licenseKey)
+//                                .useLicenseKey(true)
+//                                .auditLoggingEnabled(true)
+//                                .build());
 
         PublishModel model = new PublishModel();
 
@@ -42,13 +71,13 @@ public class KafkaService {
 
             if (e == null)
             {
-                System.out.println("Success");
+                logger.info("Success");
                 responseDto.setStatusCode(HttpStatus.OK.value());
                 responseDto.setMessage("Success");
             }
             else
             {
-                System.out.println(e);
+                logger.info(e.getMessage());
                 responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
                 responseDto.setMessage(e.getMessage());
             }
